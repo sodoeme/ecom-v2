7311521ad69bb9df460edbe13508b90c88a56e7b
sodoeme <sodoeme@gmail.com>
2023-07-18 15:17:32 -0400

    deploy

--- front-end/src/app/api/apiSlice.js
+++ front-end/src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import{createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
 
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://successfulta-api.onrender.com' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500' }),
     tagTypes:['User','Product', 'Favorites' ],
     endpoints: builder =>({})
 
