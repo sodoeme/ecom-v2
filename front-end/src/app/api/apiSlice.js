5cdf3db7e3efc4e6de6a39a481f85fb8bdff4b7c
sodoeme <sodoeme@gmail.com>
2023-07-31 20:38:36 -0400

    tests

--- front-end/src/app/api/apiSlice.js
+++ front-end/src/app/api/apiSlice.js
@@ -1,8 +1,8 @@
 import{createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
-
+const url = process.env.BASEURL || "http://localhost:3500"
 export const apiSlice = createApi({
 
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://successfultr-api.onrender.com' }),
+    baseQuery: fetchBaseQuery({ baseUrl: url }),
     tagTypes:['User','Product', 'Favorites' ],
     endpoints: builder =>({})
 
